f1cf3baea4c3f68bb5aa74c0e59615b2bbdfc558
Daniel Orio <danielislas3@live.com>
2020-01-17 20:04:37 -0600

    calculadora

---
+++ Modulo-2/Modulos/index.js
@@ -0,0 +1,10 @@
+const is = require ('is-thirteen')
+
+const suma =(n1,n2)=>n1+n2
+const resta =(n1,n2)=>n1-n2
+const div =(n1,n2)=>n1/n2
+const mult =(n1,n2)=>n1*n2
+
+console.log(resta(4,4))
+
+export { resta }; 
