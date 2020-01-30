b4f86f5c437b6c1a99c42714de7a2573baa51a45
Daniel Orio <danielislas3@live.com>
2020-01-29 21:35:40 -0600

    primer server con express

--- Modulo-3/PrimerServerExpress/index.js
+++ Modulo-3/PrimerServerExpress/index.js
@@ -1,11 +1,14 @@
 const express= require('express')
+const path = require('path')
 const port=3000
 const app = express()
 
 app.get('/',(req,res)=>{
   res.send('Hola mundo con express')
 })
-
+app.get('/index',(req,res)=>{
+  res.sendFile(path.join(__dirname+'/index.html'))
+})
 
 app.listen(port,()=>{
   console.log('Servidor listo y escuchando en el puerto: '+port)
