faf99735423504102d6d4cb1ac5f51eb125b4110
Daniel Orio <danielislas3@live.com>
2020-01-30 21:27:32 -0600

    request y response

--- Modulo-3/PrimerServerExpress/index.js
+++ Modulo-3/PrimerServerExpress/index.js
@@ -1,7 +1,12 @@
 const express= require('express')
 const path = require('path')
+const bodyParser= require('body-parser')
+
 const port=3000
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended:true}))
+
 
 app.get('/',(req,res)=>{
   res.send('Hola mundo con express')
@@ -9,6 +14,16 @@ app.get('/',(req,res)=>{
 app.get('/index',(req,res)=>{
   res.sendFile(path.join(__dirname+'/index.html'))
 })
+app.post('/form',(req,res)=>{
+  console.log(req.body.nombre)
+  res.json({nombre:req.body.nombre})
+  res.end()
+})
+app.get('/cursos/:nombre',(req,res)=>{
+  console.log(req.params.nombre)
+
+})
+
 
 app.listen(port,()=>{
   console.log('Servidor listo y escuchando en el puerto: '+port)
