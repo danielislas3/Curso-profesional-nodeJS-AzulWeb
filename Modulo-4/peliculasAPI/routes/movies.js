df29eae6d188f917d550462a968a533dcface880
danielislas3 <danielislas3@live.com>
2020-02-08 17:35:03 -0600

    ejemplo crud

--- Modulo-4/peliculasAPI/routes/movies.js
+++ Modulo-4/peliculasAPI/routes/movies.js
@@ -1,21 +1,63 @@
 const express = require('express')
-const moviesMoks= require('./utils/mocks/peliculasMocks')
+const moviesMoks = require('./utils/mocks/peliculasMocks')
 
-function moviesAPI(app){
- const router = express.Router()
-  app.use('/api/movies',router)
+function moviesAPI(app) {
+  /*CRUD*/
 
-  router.get('/',async (req,res,next)=>{
-    try{
+  /*****Read****/
+  const router = express.Router()
+  app.use('/api/movies', router)
+
+  router.get('/', async (req, res, next) => {
+    try {
       const movies = await Promise.resolve(moviesMoks)
       res.status(200).json({
-      data:movies,
-      message:'todas las peliculas'
+        data: movies,
+        message: 'todas las peliculas'
       })
-    }catch(err){
+    } catch (err) {
       next(err)
     }
   })
+  /*****Create****/
+  router.post('/', async (req, res, next) => {
+    const createdMovie = await Promise.resolve(moviesMoks)
+    try {
+      res.status(201).json({
+        data: createdMovie.movies[0].id,
+        message: 'done'
+      })
+    } catch (err) {
+      next(err)
+    }
+  })
+ /*****Update****/
+  router.put('/:movieId',async (req, res, next) => {
+    const updatedMovie = await Promise.resolve(moviesMoks)
+    try {
+      res.status(200).json({
+        data: updatedMovie.movies[0].id,
+        message: 'updated movie'
+      })
+    } catch (err) {
+      next(err)
+    }
+  })
+ /*****Delet****/
+ router.delete('/:movieId', async (req, res, next) => {
+  const deletedMovie = await Promise.resolve(moviesMoks)
+  try {
+    res.status(200).json({
+      data: deletedMovie.movies[0].id,
+      message: 'deleted movie'
+    })
+  } catch (err) {
+    next(err)
+  }
+})
+
+  
+
 }
 
-module.exports=moviesAPI
+module.exports = moviesAPI
