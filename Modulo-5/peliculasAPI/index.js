31ee69a7be6cd8daba93ed822b2ec66dc246b565
danielislas3 <danielislas3@live.com>
2020-02-23 13:15:22 -0600

    coneccion a mongo atlas mediante mongoose

--- Modulo-5/peliculasAPI/index.js
+++ Modulo-5/peliculasAPI/index.js
@@ -2,38 +2,17 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
 const moviesAPI = require('./routes/movies')
-
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
 const url = process.env.DB
-const dbName = 'movies-azul-web';
-moviesAPI(app)
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('documents');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
 
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
-  insertDocuments(db, function() {
-    client.close();
-  });
-});
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+
+kitty.save().then(() => console.log('meow'));
 
 app.listen(process.env.PORT, () => {
   console.log(` app listening on port ${process.env.PORT} 
