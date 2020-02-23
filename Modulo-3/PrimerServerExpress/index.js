05390e39110e3ad001a72873766591c71a3d8309
danielislas3 <danielislas3@live.com>
2020-02-22 23:01:01 -0600

    req y res 2

--- Modulo-3/PrimerServerExpress/index.js
+++ Modulo-3/PrimerServerExpress/index.js
@@ -1,11 +1,11 @@
 const express= require('express')
 const path = require('path')
-const bodyParser= require('body-parser')
+const bodyParser= require('body-parser');
 
 const port=3000
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
 
 
 app.get('/',(req,res)=>{
@@ -14,17 +14,30 @@ app.get('/',(req,res)=>{
 app.get('/index',(req,res)=>{
   res.sendFile(path.join(__dirname+'/index.html'))
 })
+
 app.post('/form',(req,res)=>{
-  console.log(req.body.nombre)
-  res.json({nombre:req.body.nombre})
+  const {nombre,edad} = req.body
+  console.log('Hola '+nombre+ edad)
+})
+
+app.get('/cursos/:curso',(req,res)=>{
+const {curso}= req.params
+
+switch(curso){
+
+  case('node'): res.send('Bienvenido al curso profesional de node')
+  break
+  case('react'): res.send('Bienvenido al curso profesional de reactJS')
+  break
+  case('api'): 
+  res.json({nombre:'curso de apis', duracion:'120m'})
   res.end()
+  break
+  default: res.send('Ups este curso no existe')
+}
+
+
 })
-app.get('/cursos/:nombre',(req,res)=>{
-  console.log(req.params.nombre)
-
-})
-
-
 app.listen(port,()=>{
   console.log('Servidor listo y escuchando en el puerto: '+port)
 })
